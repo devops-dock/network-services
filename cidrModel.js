a01d2979e4d2815497f928d3177f2fae908ee352
sravya <sravya@crunchops.com>
2024-03-13 17:05:37 +0530

    removed cidr model

--- cidrModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const cidrSchema = mongoose.mongoose.Schema({
-    
-});
-
-const Cidr = mongoose.model('CidrNetwork', cidrSchema);
-
-module.exports = Cidr;
